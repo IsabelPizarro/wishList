2e0ba3dfdc431d3783fbb61c700956652983142f
IsabelPizarro <chabelipizarro1@gmail.com>
2019-10-22 11:31:01 +0200

    add build again

---
+++ docs/precache-manifest.405bac3d37acbde8025e5d82aff29986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14882427a5f78c3287ae1744e15a3a89",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27f8fb0db4926768d200",
+    "url": "./static/css/main.1117e683.chunk.css"
+  },
+  {
+    "revision": "8cf2d62c1198b9eb8886",
+    "url": "./static/js/2.f27a4f0c.chunk.js"
+  },
+  {
+    "revision": "27f8fb0db4926768d200",
+    "url": "./static/js/main.63d963a2.chunk.js"
+  },
+  {
+    "revision": "e310cb5a92c0e2b4d924",
+    "url": "./static/js/runtime-main.41c7d68f.js"
+  }
+]);
